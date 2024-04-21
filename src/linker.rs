5c9fc2286cb3efc9ccc74438de32e63a9c17e51c
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-04-21 15:51:58 +0700

    feat: add EVM modules linking function

--- src/linker.rs
+++ src/linker.rs
@@ -16,4 +16,15 @@ extern "C" {
     /// Destroys the source module, returns true on error. Use the diagnostic
     /// handler to get any diagnostic message.
     pub fn LLVMLinkModules2(Dest: LLVMModuleRef, Src: LLVMModuleRef) -> LLVMBool;
+
+    /// Link EVM modules.
+    ///
+    /// Is supposed to link both EVM deploy and runtime code, and CREATE dependencies.
+    pub fn LLVMLinkMemoryBuffers(
+        InMemBufs: *const LLVMMemoryBufferRef,
+        NumInBufs: ::libc::c_uint,
+        OutMemBuf: LLVMMemoryBufferRef,
+        LldArgs: *const *const ::libc::c_char,
+        NumLldArgs: ::libc::c_uint,
+    ) -> LLVMBool;
 }
