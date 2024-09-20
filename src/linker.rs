c0c0d150e465f901854b566f82adbb9b83146d79
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-09-20 12:28:36 +0800

    feat: add library linking args (#9)
    # What ❔
    
    Adds linker API arguments for post-compile-time linkage.
    
    ## Why ❔
    
    Required for EVM equivalence:
    https://github.com/matter-labs/era-compiler-solidity/issues/102
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [x] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [x] Tests for the changes have been added / updated.
    - [x] Documentation comments have been added / updated.

--- src/linker.rs
+++ src/linker.rs
@@ -13,8 +13,14 @@ pub enum LLVMLinkerMode {
 }
 
 extern "C" {
+    /// Link the source module into the destination module.
+    ///
+    /// Destroys the source module, returns true on error. Use the diagnostic
+    /// handler to get any diagnostic message.
+    pub fn LLVMLinkModules2(Dest: LLVMModuleRef, Src: LLVMModuleRef) -> LLVMBool;
+
     /// Translate textual assembly to object code.
-    /// 
+    ///
     /// The unlinked EraVM bytecode is written to `OutMemBuf`, which must then be
     /// passed to `LLVMLinkEraVM` for linkage.
     pub fn LLVMAssembleEraVM(
@@ -24,12 +30,12 @@ extern "C" {
         ErrorMessage: *mut *mut ::libc::c_char,
     ) -> LLVMBool;
 
-    /// Disassembles the bytecode passed in `InBuffer`` starting at the offset `PC`.
-    /// 
+    /// Disassemble the bytecode passed in `InBuffer`` starting at the offset `PC`.
+    ///
     /// The result is returned via `OutBuffer``.
     /// In case of an error the function returns 'true' and an error message is passed
     /// via `ErrorMessage``. The message should be disposed with `LLVMDisposeMessage`.
-    /// 
+    ///
     /// Added in LLVM patch: https://github.com/matter-labs/era-compiler-llvm/pull/692
     pub fn LLVMDisassembleEraVM(
         TargetMachine: LLVMTargetMachineRef,
@@ -40,26 +46,39 @@ extern "C" {
         ErrorMessage: *mut *mut ::libc::c_char,
     ) -> LLVMBool;
 
+    /// Check whether the memory buffer is a valid ELF binary.
+    pub fn LLVMIsELFEraVM(InMemBuf: LLVMMemoryBufferRef) -> LLVMBool;
+
+    /// Add metadata to the ELF wrapper.
+    pub fn LLVMAddMetadataEraVM(
+        InMemBuf: LLVMMemoryBufferRef,
+        MetadataPtr: *const ::libc::c_char,
+        MetadataSize: ::libc::c_uint,
+        OutMemBuf: *mut LLVMMemoryBufferRef,
+        ErrorMessage: *mut *mut ::libc::c_char,
+    ) -> LLVMBool;
+
     /// Check if the bytecode fits into the EraVM size limit.
     pub fn LLVMExceedsSizeLimitEraVM(
         InMemBuf: LLVMMemoryBufferRef,
         MetadataSize: ::libc::c_uint,
     ) -> LLVMBool;
-    
+
+    /// Return unresolved symbols from the ELF wrapper.
+    pub fn LLVMGetUndefinedLinkerSymbolsEraVM(
+        InMemBuf: LLVMMemoryBufferRef,
+        LinkerSymbolsSize: *mut ::libc::c_uint,
+    ) -> *const *const ::libc::c_char;
+
     /// Link EraVM module.
     ///
-    /// Removes the ELF wrapper from an EraVM module.
+    /// Removes the ELF wrapper from an EraVM module if all symbols are resolved.
     pub fn LLVMLinkEraVM(
         InMemBuf: LLVMMemoryBufferRef,
         OutMemBuf: *mut LLVMMemoryBufferRef,
-        MetadataPtr: *const ::libc::c_char,
-        MetadataSize: ::libc::c_uint,
+        LinkerSymbols: *const *const ::libc::c_char,
+        LinkerSymbolValues: *const ::libc::c_char,
+        LinkerSymbolsSize: ::libc::c_uint,
         ErrorMessage: *mut *mut ::libc::c_char,
     ) -> LLVMBool;
-    
-    /// Link the source module into the destination module.
-    ///
-    /// Destroys the source module, returns true on error. Use the diagnostic
-    /// handler to get any diagnostic message.
-    pub fn LLVMLinkModules2(Dest: LLVMModuleRef, Src: LLVMModuleRef) -> LLVMBool;
 }
