2a644dec3716fd4898b2df080d3177a18897a638
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-09-23 19:39:06 +0800

    fix: linker API integer types

--- src/linker.rs
+++ src/linker.rs
@@ -19,6 +19,17 @@ extern "C" {
     /// handler to get any diagnostic message.
     pub fn LLVMLinkModules2(Dest: LLVMModuleRef, Src: LLVMModuleRef) -> LLVMBool;
 
+    /// Link EVM modules.
+    ///
+    /// Is supposed to link both EVM deploy and runtime code, and CREATE dependencies.
+    pub fn LLVMLinkMemoryBuffers(
+        InMemBufs: *const LLVMMemoryBufferRef,
+        NumInBufs: u64,
+        OutMemBuf: *mut LLVMMemoryBufferRef,
+        LldArgs: *const *const ::libc::c_char,
+        NumLldArgs: u64,
+    ) -> LLVMBool;
+
     /// Translate textual assembly to object code.
     ///
     /// The unlinked EraVM bytecode is written to `OutMemBuf`, which must then be
@@ -40,8 +51,8 @@ extern "C" {
     pub fn LLVMDisassembleEraVM(
         TargetMachine: LLVMTargetMachineRef,
         InMemBuf: LLVMMemoryBufferRef,
-        PC: ::libc::c_uint,
-        Options: ::libc::c_uint,
+        PC: u32,
+        Options: u32,
         OutMemBuf: *mut LLVMMemoryBufferRef,
         ErrorMessage: *mut *mut ::libc::c_char,
     ) -> LLVMBool;
@@ -53,21 +64,18 @@ extern "C" {
     pub fn LLVMAddMetadataEraVM(
         InMemBuf: LLVMMemoryBufferRef,
         MetadataPtr: *const ::libc::c_char,
-        MetadataSize: ::libc::c_uint,
+        MetadataSize: u64,
         OutMemBuf: *mut LLVMMemoryBufferRef,
         ErrorMessage: *mut *mut ::libc::c_char,
     ) -> LLVMBool;
 
     /// Check if the bytecode fits into the EraVM size limit.
-    pub fn LLVMExceedsSizeLimitEraVM(
-        InMemBuf: LLVMMemoryBufferRef,
-        MetadataSize: ::libc::c_uint,
-    ) -> LLVMBool;
+    pub fn LLVMExceedsSizeLimitEraVM(InMemBuf: LLVMMemoryBufferRef, MetadataSize: u64) -> LLVMBool;
 
     /// Return unresolved symbols from the ELF wrapper.
     pub fn LLVMGetUndefinedLinkerSymbolsEraVM(
         InMemBuf: LLVMMemoryBufferRef,
-        LinkerSymbolsSize: *mut ::libc::c_uint,
+        LinkerSymbolsSize: *mut u64,
     ) -> *const *const ::libc::c_char;
 
     /// Link EraVM module.
@@ -78,7 +86,7 @@ extern "C" {
         OutMemBuf: *mut LLVMMemoryBufferRef,
         LinkerSymbols: *const *const ::libc::c_char,
         LinkerSymbolValues: *const ::libc::c_char,
-        LinkerSymbolsSize: ::libc::c_uint,
+        LinkerSymbolsSize: u64,
         ErrorMessage: *mut *mut ::libc::c_char,
     ) -> LLVMBool;
 }
