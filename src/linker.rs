4b0421ff90757ed7a701ce5b81926515605dd4bd
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-10-09 22:46:50 +0800

    feat: add EVM modules linking function (#5)

--- src/linker.rs
+++ src/linker.rs
@@ -19,6 +19,35 @@ extern "C" {
     /// handler to get any diagnostic message.
     pub fn LLVMLinkModules2(Dest: LLVMModuleRef, Src: LLVMModuleRef) -> LLVMBool;
 
+    /// Links the deploy and runtime ELF object files using the information about
+    /// dependencies.
+    /// `inBuffers` - array of input memory buffers with following structure:
+    ///
+    /// `inBuffers[0]` - deploy ELF object code
+    /// `inBuffers[1]` - deployed (runtime) ELF object code
+    /// --------------------------
+    /// `inBuffers[2]` - 1-st sub-contract (final EVM bytecode)
+    /// ...
+    /// `inBuffers[N]` - N-st sub-contract (final EVM bytecode)
+    ///
+    /// Sub-contracts are optional. They should have the same ordering as in
+    /// the Yul layout.
+    ///
+    /// `inBuffersIDs` - array of string identifiers of the buffers. IDs correspond
+    /// to the object names in the Yul layout.
+    /// On success, `outBuffers[0]` will contain the deploy bytecode and `outBuffers[1]`
+    /// the runtime bytecode.
+    /// In case of an error the function returns `true` and the error message is
+    /// passes in `ErrorMessage`. The message should be disposed by
+    /// `LLVMDisposeMessage`.
+    pub fn LLVMLinkEVM(
+        InMemBufs: *const LLVMMemoryBufferRef,
+        InMemBufIDs: *const *const ::libc::c_char,
+        NumInBufs: u64,
+        OutMemBufs: *mut [LLVMMemoryBufferRef; 2],
+        ErrorMessage: *mut *mut ::libc::c_char,
+    ) -> LLVMBool;
+
     /// Translate textual assembly to object code.
     ///
     /// The unlinked EraVM bytecode is written to `OutMemBuf`, which must then be
